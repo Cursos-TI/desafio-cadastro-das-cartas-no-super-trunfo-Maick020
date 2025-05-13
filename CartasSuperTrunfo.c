fcef40df6ec4eea22cb7c046666e9f02d7260075
Maick020 <cardosomaick864@gmail.com>
2025-05-13 20:59:51 +0000

    trabalho supertunfov2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,12 +7,13 @@
 //Teste larissa
 
 int main() {
-char codigo[5];
-char estado[10]; 
-int populacao;
-float area;
-float pib;
-int pontostur;
+char codigo[5] , codigo2[5] ;
+char estado[10], estado2[10] ; 
+int populacao, populacao2 ;
+float area , area2;
+float pib, pib2;
+int pontostur, pontostur2;
+int dp = area / populacao;
 
 printf("qual e o estado:\n");
 scanf("%s", &estado);
@@ -30,10 +31,31 @@ printf("qual e o numero de pontos turisticos:\n");
 scanf("%d", &pontostur);
 
 printf("qual e o codigo\n");
-scanf("%s", codigo);
+scanf("%s\n", &codigo);
 
-printf("Carta 1 \n%s\n %d\n %f\n %f\n %d\n %d\n %s\n",estado , populacao,area,pib,pontostur,codigo);
 
+printf("Carta 1 \n estado:%s\n populaçao:%d\n area:%f\n pib:%f\n pontos turisticos:%d\n codigo:%s\n dp:%d\n ",estado,populacao,area,pib,pontostur,codigo,dp);
+
+
+//printf("qual e o  estado 2:\n");
+//scanf("%s", &estado2);
+
+//printf("qual e a   populaçao 2:\n");
+//scanf("%d", &populacao2);
+
+//printf("quala a area 2:\n");
+//scanf("%f", &area2);
+
+//printf("qual o  pib 2:\n");
+//scanf("%f", &pib2);
+
+//printf("qual e o numero de pontos turisticos 2:\n");
+//scanf("%d", &pontostur);
+
+//printf("qual e o codigo 2:\n");
+//scanf("%s", codigo);
+
+//printf("Carta 2 \n%s\n %d\n %f\n %f\n %d\n %d\n %s\n",estado2 , populacao2,area2,pib2,pontostur2,codigo2);
 
     return 0;
 }
